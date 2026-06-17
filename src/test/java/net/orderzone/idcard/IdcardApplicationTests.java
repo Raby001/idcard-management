69a9f88ad2ecd22b003f586cf8bc748e1b62ff49
Raby001 <hanraby001@gmail.com>
2026-06-17 20:47:48 +0700

    Add ansible deployment files and test configuration

--- src/test/java/net/orderzone/idcard/IdcardApplicationTests.java
+++ src/test/java/net/orderzone/idcard/IdcardApplicationTests.java
@@ -1,13 +1,11 @@
 package net.orderzone.idcard;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class IdcardApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
